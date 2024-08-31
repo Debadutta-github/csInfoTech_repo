be2d706bbdd2fe848f0ebf721e5060e57d84fd7f
debad <debad@Debadutta>
2024-08-31 16:27:26 +0530

    first push

--- CsInfoTechSolutions_Assesment/src/Test01UI/ChromeBrowserScreenshots.java
+++ CsInfoTechSolutions_Assesment/src/Test01UI/ChromeBrowserScreenshots.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.io.FileHandler;
 
-public class ChromeBrowserURL {
+public class ChromeBrowserScreenshots {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		WebDriver driver = new ChromeDriver();
